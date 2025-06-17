774fc93b9fa8e3e559293b9067acf6a89574765a
AyaAly9 <ayaaly929@gmail.com>
2025-06-17 21:50:27 +0300

    changes background random

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let landingPage= document.querySelector('.landing-page');
+let imgsArray = ['img1.jpg','img3.jpg','img4.jpg'];
+
+setInterval(()=>{
+    let randomIndx = Math.floor(Math.random() * imgsArray.length);
+
+    landingPage.style.backgroundImage = 'url("imgs/'+ imgsArray[randomIndx] + ' ")';
+
+},3000);
